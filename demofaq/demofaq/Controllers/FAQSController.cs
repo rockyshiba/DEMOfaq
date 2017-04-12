5d7ccca10328c3958cb0793547f4ed4d68436433
rockyshiba <leejsitu@gmail.com>
2017-04-12 01:54:51 -0400

    Base application with cRUD controller

---
+++ demofaq/demofaq/Controllers/FAQSController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using demofaq.Models;
+
+namespace demofaq.Controllers
+{
+    public class FAQSController : Controller
+    {
+        private faqdemodbContext db = new faqdemodbContext();
+
+        // GET: FAQS
+        public ActionResult Index()
+        {
+            return View(db.FAQS.ToList());
+        }
+
+        // GET: FAQS/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FAQS fAQS = db.FAQS.Find(id);
+            if (fAQS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fAQS);
+        }
+
+        // GET: FAQS/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FAQS/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Question,Answer,Name,Email")] FAQS fAQS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FAQS.Add(fAQS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(fAQS);
+        }
+
+        // GET: FAQS/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FAQS fAQS = db.FAQS.Find(id);
+            if (fAQS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fAQS);
+        }
+
+        // POST: FAQS/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Question,Answer,Name,Email,Created_at")] FAQS fAQS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(fAQS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fAQS);
+        }
+
+        // GET: FAQS/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FAQS fAQS = db.FAQS.Find(id);
+            if (fAQS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fAQS);
+        }
+
+        // POST: FAQS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FAQS fAQS = db.FAQS.Find(id);
+            db.FAQS.Remove(fAQS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ demofaq/demofaq/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="demofaq.MvcApplication" Language="C#" %>
