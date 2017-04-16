e62f7e30b924985344e3d5f0e868d480b3da5e91
rockyshiba <leejsitu@gmail.com>
2017-04-16 04:02:44 -0400

    Added layout css. Home/Index an example of Tier1

--- demofaq/demofaq/Controllers/HomeController.cs
+++ demofaq/demofaq/Controllers/HomeController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using demofaq.Models;
 
 namespace demofaq.Controllers
 {
     public class HomeController : Controller
     {
+        faqdemodbContext db = new faqdemodbContext();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(db.FAQS.ToList());
         }
     }
 }
